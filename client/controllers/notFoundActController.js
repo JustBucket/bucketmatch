d184ddf8317c27bd45fcd22366f90ebb4972b322
dianacheung <diana.cheung.2013@gmail.com>
2016-09-14 14:52:17 -0700

    begin angular add new dream post

--- client/controllers/notFoundActController.js
+++ client/controllers/notFoundActController.js
@@ -3,6 +3,18 @@ angular
   .controller('notFoundActController', notFoundActController);
 
 function notFoundActController($scope, $location) {
+  $scope.postNewActivity = function() {
+    //console.log('in postNewActivity', $scope.activityname, $scope.activitydesc);
+    var newActivityObj = {};
+    var activityName = $scope.activityname.trim();
+    var activityDesc = $scope.activitydesc.trim();
 
+    // simple validation checks
+
+    var postUrl = '/activity/add';
+    // $.post(postUrl, )
+    //   .then()
+    //   .catch();
+  };
 }
 
