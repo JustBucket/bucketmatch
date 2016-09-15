9ebeabaae2ae666204e1abf5e2e47fee7f60eb0b
dianacheung <diana.cheung.2013@gmail.com>
2016-09-14 19:37:23 -0700

    added httpOnly: true

--- server-sql/controllers/cookieController.js
+++ server-sql/controllers/cookieController.js
@@ -1,10 +1,9 @@
-
-let cookieController = {};
+var cookieController = {};
 cookieController.setCookie = setCookie;
 
 function setCookie(req, res, next) {
-	res.cookie('userID', req.body.user_id);
-	res.cookie('token', req.body.access_token);
+	res.cookie('userID', req.body.user_id, {httpOnly: true});
+	res.cookie('token', req.body.access_token, {httpOnly: true});
 	next()
 }
 
