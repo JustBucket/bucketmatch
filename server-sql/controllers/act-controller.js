61fec0b74f31690c76ae62947a34fe9ff96ef15e
dianacheung <diana.cheung.2013@gmail.com>
2016-09-14 19:40:05 -0700

    modified add method in prep of new custom activity

--- server-sql/controllers/act-controller.js
+++ server-sql/controllers/act-controller.js
@@ -11,6 +11,21 @@ function index(req, res) { // retruns a list of all activities
 }
 
 function add(req, res, next) { // adds a new activity to the database
+  //console.log("in act-controller add(): req.body", req.body);
+  var newActivityObj = {actname: req.body.actname, actdesc: req.body.actdesc};
+
+  Activity.create(newActivityObj)
+    .then((resp) => {
+      console.log(resp);
+      req.actKey = resp.dataValues._id;
+    })
+    .catch((err) => {
+      if (err) console.error(err);
+    });
+  next();
+}
+/*
+function add(req, res, next) { // adds a new activity to the database
   Activity.create(req.body.data.event)
     .then((resp) => {
       console.log(resp);
@@ -21,6 +36,7 @@ function add(req, res, next) { // adds a new activity to the database
     });
   next();
 }
+*/
 
 function show(req, res, next) { // finds a single activity
   Activity.find(req.body[0], err => {
