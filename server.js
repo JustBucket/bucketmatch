a5cada4140dace7800b330397bc20cdd09d65b87
dianacheung <diana.cheung.2013@gmail.com>
2016-09-14 19:37:50 -0700

    called addNew in /activity/add

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const uaCtrl = require('./server-sql/controllers/ua-controller');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const cookieController = require('./server-sql/controllers/cookieController')
+const cookieController = require('./server-sql/controllers/cookieController');
 
 const app = express();
 
@@ -27,11 +27,13 @@ app.get('/user/:username/:password', userCtrl.show, userCtrl.conn); // to log in
 app.get('/userinfo/:username/', userCtrl.profile, (req, res) => { res.end(); }); // to get a single user's profile with limited info'
 
 app.get('/test', userCtrl.index); // full list of users, not needed for front-end
-app.get('/fblogin', userCtrl.getToken, userCtrl.getClientId, cookieController.setCookie, function(req, res) {res.end()})
+app.get('/fblogin', userCtrl.getToken, userCtrl.getClientId, cookieController.setCookie, function(req, res) {
+  res.redirect('/#/profile');
+});
 app.post('/user/add', userCtrl.add, (req, res) => { res.end(); });// to add a single user
 
 app.get('/activities', actCtrl.index); // full list of activities, for user to choose from
-app.post('/activity/add', actCtrl.add, uaCtrl.add, (req, res) => { res.end(); });// to add a new activity
+app.post('/activity/add', actCtrl.add, uaCtrl.addNew, (req, res) => { res.end(); });// to add a new activity
 
 app.get('/useractivities', uaCtrl.index, (req, res) => { res.end(); });// to view all joins between users & activities
 app.post('/useractivity/add', uaCtrl.add, (req, res) => { res.end(); });// to add an existing activity TO a User
